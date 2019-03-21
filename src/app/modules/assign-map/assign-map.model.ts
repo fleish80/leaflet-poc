bee5555fde17eb73ad861ab0e410945ea2ea9ace
Fleish Dmitry <Fleish.D@gmail.com>
2019-03-21 18:22:45 +0200

    Yeeeea

--- src/app/modules/assign-map/assign-map.model.ts
+++ src/app/modules/assign-map/assign-map.model.ts
@@ -3,11 +3,10 @@ import {WingItem} from '../../core/models/items/wing.item';
 
 
 export class AssignMap {
-  // availableMaps: WingItem[];
 
-
+  availableMaps: WingItem[];
 
   constructor(building?: any, availableMaps?: WingItem[]) {
-
+    this.availableMaps = availableMaps.sort((map1: WingItem, map2: WingItem) => map1.mapName >= map2.mapName ? 1 : -1);
   }
 }
