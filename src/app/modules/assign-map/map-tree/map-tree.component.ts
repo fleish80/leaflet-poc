d0b4ad5dc7f7909d5987c2a75e3082e13eb664d5
Fleish Dmitry <Fleish.D@gmail.com>
2019-04-07 11:25:24 +0300

    Assign map

--- src/app/modules/assign-map/map-tree/map-tree.component.ts
+++ src/app/modules/assign-map/map-tree/map-tree.component.ts
@@ -1,7 +1,8 @@
-import {ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {NestedTreeControl} from '@angular/cdk/tree';
 import {ArrayDataSource} from '@angular/cdk/collections';
 import {TreeNode} from './tree-node.model';
+import {WingMapItem} from '../../../core/models/items/wing-map.item';
 
 @Component({
   selector: 'mv-map-tree',
@@ -12,11 +13,12 @@ import {TreeNode} from './tree-node.model';
 export class MapTreeComponent implements OnChanges {
 
   @Input() treeNodes: TreeNode[];
+  @Input() data: WingMapItem[];
   treeControl: NestedTreeControl<TreeNode>;
   dataSource: ArrayDataSource<TreeNode>;
   hasChild: (_: number, TreeNode) => boolean;
 
-  constructor() {
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -24,6 +26,8 @@ export class MapTreeComponent implements OnChanges {
       this.treeControl = new NestedTreeControl<TreeNode>((node: any) => node.children);
       this.dataSource = new ArrayDataSource(this.treeNodes);
       this.hasChild =  (_: number, node: TreeNode) => !!node.children && node.children.length > 0;
+      this.treeControl.dataNodes = this.treeNodes;
+      this.treeControl.expandAll();
     }
   }
 
